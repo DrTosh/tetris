4a9dd0ae726fb89dc7e1e01d3fcd292ef464ea50
DrTosh <DoktorTosh@gmail.com>
2020-03-31 20:46:42 +0200

    update methods, move left, right, down and rotate

--- src/active_tetromino.rs
+++ src/active_tetromino.rs
@@ -1,5 +1,5 @@
 use crate::tetromino::Tetromino;
-use crate::tetris::Screen;
+use crate::tetris::{Screen, BLOCK_SIZE_X, BLOCK_SIZE_Y};
 use crate::rotation::Rotation;
 
 #[derive(Debug)]
@@ -13,7 +13,7 @@ pub struct ActiveTetromino {
 impl ActiveTetromino {
     pub fn new(pos_x: usize, pos_y: usize) -> ActiveTetromino {
         ActiveTetromino {
-            tetromino: Tetromino::O,
+            tetromino: Tetromino::L,
             rotation: Rotation::DEGREE_0,
             pos_x,
             pos_y,
@@ -23,12 +23,31 @@ impl ActiveTetromino {
     pub fn update(&mut self, screen: &mut Screen, pos_x: usize, pos_y: usize, rotation: Rotation) {
         self.tetromino.update(screen, self.pos_x, self.pos_y, self.rotation, true);
 
-        self.pos_x = pos_x;
-        self.pos_y = pos_y;
-        self.rotation = rotation;
+        if !self.tetromino.collide(screen, pos_x, pos_y, rotation) {
 
-        crate::Tetris::log(format!("{:?}", self));
+            self.pos_x = pos_x;
+            self.pos_y = pos_y;
+            self.rotation = rotation;
 
-        self.tetromino.update(screen, pos_x, pos_y, rotation, false);
+            self.tetromino.update(screen, pos_x, pos_y, rotation, false);
+        } else {
+            self.tetromino.update(screen, self.pos_x, self.pos_y, self.rotation, false);
+        }
+    }
+        
+    pub fn rotate(&mut self, screen: &mut Screen) {
+        self.update(screen, self.pos_x, self.pos_y, self.rotation.next());
+    }
+
+    pub fn move_right(&mut self, screen: &mut Screen) {
+        self.update(screen, self.pos_x + BLOCK_SIZE_X, self.pos_y, self.rotation);
+    }
+
+    pub fn move_left(&mut self, screen: &mut Screen) {
+        self.update(screen, self.pos_x - BLOCK_SIZE_X, self.pos_y, self.rotation);
+    }
+
+    pub fn move_down(&mut self, screen: &mut Screen) {
+        self.update(screen, self.pos_x, self.pos_y + BLOCK_SIZE_Y, self.rotation);
     }
 }
