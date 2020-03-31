e0ee4147e7ea2a4afd9f326451f81f62686a39ae
DrTosh <DoktorTosh@gmail.com>
2020-03-31 13:47:13 +0200

    enum rotation has new function to get next rotation

--- src/rotation.rs
+++ src/rotation.rs
@@ -1,6 +1,18 @@
+#[derive(Debug, Copy, Clone)]
 pub enum Rotation {
     DEGREE_0,
     DEGREE_90,
     DEGREE_180,
     DEGREE_270
+}
+
+impl Rotation {
+    pub fn next(&self) -> Rotation {
+        match self {
+            Rotation::DEGREE_0 => Rotation::DEGREE_90,
+            Rotation::DEGREE_90 => Rotation::DEGREE_180,
+            Rotation::DEGREE_180 => Rotation::DEGREE_270,
+            Rotation::DEGREE_270 => Rotation::DEGREE_0,
+        }
+    }
 }
